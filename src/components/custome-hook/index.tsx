c34535562079b19ce898a5cbca6396190c6ab9e9
shawon <rabby.shawon@vivasoftltd.com>
2024-05-28 12:30:14 +0600

    Added react custom hook

--- src/components/custome-hook/index.tsx
+++ src/components/custome-hook/index.tsx
@@ -1,7 +1,5 @@
 //React official doc for better understanding  https://legacy.reactjs.org/docs/hooks-custom.html
 
-//For HOC ref: https://medium.com/@abhiman.max_44888/supercharge-your-react-components-with-higher-order-components-hocs-and-react-hooks-454eabb17de4#:~:text=Higher%2DOrder%20Components%20(HOCs),-We'll%20start&text=HOCs%20are%20a%20pattern%20in,with%20additional%20props%20or%20behavior.
-
 import { useState } from "react";
 import useDocTitle from "../../hooks/useDocTitle";
 
